46c01ee283522b0061b36cd69ac1cceaa50a6ef3
Mack D <mckenna.duvall@gmail.com>
2021-06-12 14:17:59 -0700

    Transforming and Loading

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 drop table vaccinations;
 
 CREATE TABLE vaccinations (
-date date PRIMARY KEY,
+date date,
 state TEXT,
 people_vaccinated INT,
 people_fully_vaccinated INT
@@ -9,8 +9,10 @@ people_fully_vaccinated INT
 
 select * from vaccinations;
 
+drop table us_mobility_2021;
+
 CREATE TABLE us_mobility_2021 (
-    date date PRIMARY KEY,
+    date date,
     state TEXT,
     retail_and_rec_percent_change INT,
     grocery_and_pharmacy_percent_change INT,
